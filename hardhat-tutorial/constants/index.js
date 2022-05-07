d3519cebaeb7604cd8b2664244a1a16754e8209a
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2022-05-07 14:01:48 -0400

    fixed typo

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -6,4 +6,4 @@ const METADATA_URL = "https://nft-collection-one-flax.vercel.app/api/";
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
 // Crypto Devs Contract Address: 0x7D8053980D8A07A6400cAb56984D9B3A37379
-// MFT Contract Address: 0xdC3A6Fcf2DacB5DC69fc944f1c036d5a7f2D0876
+// NFT Contract Address: 0xdC3A6Fcf2DacB5DC69fc944f1c036d5a7f2D0876
