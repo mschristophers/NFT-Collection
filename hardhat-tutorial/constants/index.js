409ee860475f8eb0515cd927e4e8506c7aed7a32
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2022-05-07 14:00:32 -0400

    some updates to NFT contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,8 +1,9 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x48741987D290BCa5464Aa84eE40bD43866C3Ea14";
+const WHITELIST_CONTRACT_ADDRESS = "0xdC3A6Fcf2DacB5DC69fc944f1c036d5a7f2D0876";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-one-flax.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
-// Crypto Devs Contract Address: 0x7D8053980D8A07A6400cAb56984D9B3A37379E3d
+// Crypto Devs Contract Address: 0x7D8053980D8A07A6400cAb56984D9B3A37379
+// MFT Contract Address: 0xdC3A6Fcf2DacB5DC69fc944f1c036d5a7f2D0876
